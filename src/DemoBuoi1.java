b0a512971d1e646fa8001ff0e0fd4dbd0f896df9
trongkha256 <trongkha256@gmail.com>
2022-08-29 18:42:21 +0700

    feat: add new java file

--- src/DemoBuoi1.java
+++ src/DemoBuoi1.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class DemoBuoi1 {
+import java.util.Scanner;
+
+public class DemoBuoi1 {
+    public static void main(String[] arg) {
+//        Scanner scanner=new Scanner(System.in);
+//        String inputData = scanner.next();
+//        System.out.printf("Test "+ inputData);
+        Scanner scanner = new Scanner(System.in);
+        System.out.printf("Nhập vào điểm trung bình: ");
+        int diemToan = scanner.nextInt();
+        if (diemToan >= 9 && diemToan <= 10) {
+            System.out.printf("Giỏi");
+        } else if (diemToan >= 8) {
+            System.out.printf("Khá");
+
+        } else if (diemToan >= 7) {
+            System.out.printf("Tiên Tiến");
+        } else if (diemToan >= 5) {
+            System.out.printf("Trung Bình");
+        } else System.out.printf("Yếu");
+    }
+
+
 }
--- src/DemoBuoi2.java
+++ src/DemoBuoi2.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class DemoBuoi2 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DemoBuoi2 {
+    public  static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int[] arr= new int[5];
+        int n=arr.length;
+        for (int i=0;i<n;i++){
+            arr[i]= scanner.nextInt();
+        }
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+
+        System.out.println();
+
+    }
 }
